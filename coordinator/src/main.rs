3f7a6a047faa90837c1daa7a162a46580e8bf84d
William Greer <wgreer184@gmail.com>
2019-07-30 20:59:57 -0400

    Set up basic websocket server

--- coordinator/src/main.rs
+++ coordinator/src/main.rs
@@ -5,13 +5,35 @@
 #[macro_use]
 extern crate rocket;
 
+use failure::Error;
+use std::thread;
+use websocket::Message;
+use websocket::sync::Server;
+
 #[get("/")]
 /// Root web page for the coordinator.
 fn index() -> &'static str {
     "Project Circles: Coordinator"
 }
 
-/// Main thread running the web application.
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+/// Main thread running the web application and websockets server.
+fn main() -> Result<(), Error> {
+    // Spawn a thread to start web interface front end.
+    thread::spawn(move || {
+        rocket::ignite().mount("/", routes![index]).launch();
+    });
+    
+    // Start websocket server where node communication occurs.
+    let coordinator = Server::bind("0.0.0.0:9090")?;
+
+    // Spawn a thread for each websocket connection.
+    for connection in coordinator.filter_map(Result::ok) {
+        thread::spawn(move || {
+            println!("Client has connected");
+            let mut client = connection.accept().expect("Error: Connection failed");
+            let message = Message::text("Hello, client!");
+            client.send_message(&message).expect("Error: Message send failed");
+        });
+    }    
+    Ok(())
 }
